c886e1c153ce8cb053e68db1f2a140c1b48727b3
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-11-06 14:31:49 +0000

    Update

--- js/time.js
+++ js/time.js
@@ -1,15 +1,21 @@
 const timezoneSelect = document.getElementById("timezone-select");
-const selectedTimezone = timezoneSelect.value;
 const errorMsg = document.getElementById("error-msg");
 const sameZoneError = document.getElementById("same-timezone-error");
-const timezoneSpans = document.getElementsByClassName("timezone-name");
 const popup = document.getElementById("timezone-popup");
-const timezonePanel = document.getElementById("all-timezones");
 const buttonContainer = document.getElementById("buttonContainer");
 let toastBox = document.getElementById("toastbox");
-const timezonePanelArray = [];
-let currentTimeZone = [];
-let localStorageTimezone = [];
+
+let currentTimeZone = null;
+// ------------------------------------------
+// Load current timezones from localstorage
+// ------------------------------------------
+let storedTimezoneListStr = localStorage.getItem("timezones_list");
+let currentTimezones = JSON.parse(storedTimezoneListStr) || [];
+if (currentTimezones.length === 0) {
+  // To update user's current time on the main screen
+  currentTimezones.push(Intl.DateTimeFormat().resolvedOptions().timeZone);
+  currentTimeZone = currentTimezones[0];
+}
 
 // Toast Messages
 let addedTimezoneMsg =
@@ -90,7 +96,9 @@ function addSelectedTimezone() {
     return;
   }
 
+  const timezoneSpans = document.getElementsByClassName("timezone-name");
   let timezoneExists = false;
+
   for (const span of timezoneSpans) {
     if (span.innerHTML === selectedTimezone) {
       timezoneExists = true;
@@ -103,79 +111,29 @@ function addSelectedTimezone() {
     return;
   }
 
-  const currentTime = new Date().toLocaleString("en-US", {
-    timeZone: selectedTimezone,
-    hour: "numeric",
-    minute: "numeric",
-    hour12: false,
-  });
-
-  const timezoneButton = document.createElement("button");
-  timezoneButton.setAttribute(
-    "class",
-    "flex justify-between py-3 px-5 text-left tabular-nums border-b ease-in-out hover:bg-amber-300 block w-full bg-gray-100"
-  );
-  timezoneButton.innerHTML =
-    '<span class="font-semibold">' +
-    currentTime +
-    '</span> <span class="text-gray-500 timezone-name">' +
-    selectedTimezone +
-    "</span>";
-  timezonePanelArray.push(timezoneButton);
   sameZoneError.classList.add("hidden");
-
-  // Store the timezone in localStorage
-  localStorageTimezone.push(selectedTimezone);
-  let convertedString = JSON.stringify(localStorageTimezone);
-  localStorage.setItem("localStorageTimezone", convertedString);
-
-  loadElementTime(timezoneButton);
 
   $("#timezone-select").val(null).trigger("change");
 
-  timezonePanelArray.forEach((button) => {
-    button.addEventListener("click", function (event) {
-      // To clear the interval for currentTime
-      setTimeout(() => {
-        clearInterval(currentTimeInterval), 1000;
-      });
-      clearInterval(currentTimeInterval);
-      const targetBtn = event.currentTarget;
-      loadElementTime(targetBtn);
-    });
-
-    timezonePanelArray.filter(Boolean).map(function (item) {
-      return buttonContainer.appendChild(item);
-    });
-  });
-
   showToast(addedTimezoneMsg);
   closeTimezonePopup();
+
+  // Add the new timezone to timezones array and populate the timezone panel
+  currentTimezones.push(selectedTimezone);
+  populateTimezonePanel(currentTimezones)
 }
 
 function loadElementTime(targetBtn) {
-  const timezoneNameElement = targetBtn.querySelector(".timezone-name");
-  if (timezoneNameElement) {
-    setTimeout(() => {
-      clearInterval(currentTimeInterval), 1000;
-    });
-    clearInterval(currentTimeInterval);
-
-    currentTimeZone.splice(0, currentTimeZone.length);
-    const selectedTimezoneContext = timezoneNameElement.textContent;
-    currentTimeZone.push(selectedTimezoneContext);
-    updateTime();
-
-    // Add bg-amber-300 class to clicked button
-    targetBtn.classList.add("bg-amber-300");
-
-    // Remove bg-amber-300 class from other buttons
-    timezonePanelArray.forEach((otherButton) => {
-      if (otherButton !== targetBtn) {
-        otherButton.classList.remove("bg-amber-300");
-      }
-    });
+  const previouslySelectedTimezone = document.querySelector('.current-timezone');
+  if (previouslySelectedTimezone) {
+    previouslySelectedTimezone.classList.remove("current-timezone");
   }
+
+  const timezoneNameElement = targetBtn.querySelector(".timezone-name");
+  currentTimeZone = timezoneNameElement.textContent;
+  loadCurrentTimezoneTime();
+
+  targetBtn.classList.add("current-timezone");
 }
 
 // Add timezone button in the right panel
@@ -202,41 +160,19 @@ document
 
 // Delete button functionality
 document.getElementById("delete-btn").addEventListener("click", function () {
-  const leftPanelTimezone = document.getElementById("timezone");
-  if (timezonePanelArray.length > 1) {
-    for (const span of timezoneSpans) {
-      if (span.innerHTML === leftPanelTimezone.textContent) {
-        const index = timezonePanelArray.findIndex(
-          (button) => button.textContent === span.parentElement.textContent
-        );
-        if (index === 0) {
-          timezonePanelArray[index + 1].classList.add("bg-amber-300");
-          timezonePanelArray[index + 1].click();
-        } else if (index > 0) {
-          timezonePanelArray[index - 1].classList.add("bg-amber-300");
-          timezonePanelArray[index - 1].click();
-        }
-        span.parentElement.remove();
-        timezonePanelArray.splice(index, 1);
-
-        showToast(deletedTimezoneMsg);
-
-        // Update localStorage after deleting the timezone
-        const selectedTimezone = span.innerHTML;
-        const localStorageTimezone = JSON.parse(
-          localStorage.getItem("localStorageTimezone")
-        );
-        const indexToDelete = localStorageTimezone.indexOf(selectedTimezone);
-        if (indexToDelete > -1) {
-          localStorageTimezone.splice(indexToDelete, 1);
-          let convertedString = JSON.stringify(localStorageTimezone);
-          localStorage.setItem("localStorageTimezone", convertedString);
-        }
-      }
-    }
-  } else {
+  const activeTimezoneEl = document.getElementById("timezone");
+  if (currentTimezones.length === 1) {
     showToast(cantDeleteMsg);
+    return;
   }
+
+  const activeTimezoneName = activeTimezoneEl.textContent;
+  const indexToDelete = currentTimezones.indexOf(activeTimezoneName);
+
+  currentTimezones.splice(indexToDelete, 1);
+  populateTimezonePanel(currentTimezones);
+
+  showToast(deletedTimezoneMsg);
 });
 
 // Checks if the user pressed Escape key
@@ -247,40 +183,39 @@ document.addEventListener("keydown", (event) => {
 });
 
 // This function update time in main section
-function updateTime() {
-  if (currentTimeZone.length > 0) {
-    let innerCurrentTimeZonne = currentTimeZone[0];
-
-    const date = new Date().toLocaleDateString("en-US", {
-      timeZone: innerCurrentTimeZonne,
-    });
-    const time = new Date().toLocaleTimeString("en-US", {
-      timeZone: innerCurrentTimeZonne,
-      hour12: false,
-    });
-
-    document.getElementById("timezone").textContent = innerCurrentTimeZonne;
-    document.getElementById("time").textContent = time;
-
-    const formattedDate = new Date(date);
-    const formattedDateString = formattedDate.toDateString();
-    document.getElementById("date").textContent = formattedDateString;
+function loadCurrentTimezoneTime() {
+  if (!currentTimeZone) {
+    return;
   }
 
-  setInterval(updateTime, 1000);
+  let innerCurrentTimeZone = currentTimeZone;
+
+  const date = new Date().toLocaleDateString("en-US", {
+    timeZone: innerCurrentTimeZone,
+  });
+
+  const time = new Date().toLocaleTimeString("en-US", {
+    timeZone: innerCurrentTimeZone,
+    hour12: false,
+  });
+
+  document.getElementById("timezone").textContent = innerCurrentTimeZone;
+  document.getElementById("time").textContent = time;
+
+  const formattedDate = new Date(date);
+  document.getElementById("date").textContent = formattedDate.toDateString();
+
+  setInterval(loadCurrentTimezoneTime, 1000);
 }
 
 // This function populates the timezone panel with localstorage data
-function populateTimezonePanel(convertedArray) {
-  // To clear the previous interval for currentTime
-  setTimeout(() => {
-    clearInterval(currentTimeInterval), 1000;
-  });
-  clearInterval(currentTimeInterval);
+function populateTimezonePanel(timezonesList) {
+  // Remove all the existing timezones
+  buttonContainer.innerHTML = '';
 
-  currentTimeZone.push(convertedArray[0]);
-  updateTime();
-  convertedArray.forEach((timezone) => {
+  let lastTimezoneButton = null;
+
+  timezonesList.forEach((timezone) => {
     const currentTime = new Date().toLocaleString("en-US", {
       timeZone: timezone,
       hour: "numeric",
@@ -299,86 +234,28 @@ function populateTimezonePanel(convertedArray) {
       '</span> <span class="text-gray-500 timezone-name">' +
       timezone +
       "</span>";
-    timezonePanelArray.push(timezoneButton);
 
-    timezonePanelArray.filter(Boolean).map(function (item) {
-      return buttonContainer.appendChild(item);
+    buttonContainer.appendChild(timezoneButton);
+
+    timezoneButton.addEventListener("click", function (event) {
+      const targetBtn = event.currentTarget;
+      loadElementTime(targetBtn);
     });
 
-    timezonePanelArray.forEach((button) => {
-      button.addEventListener("click", function (event) {
-        // To clear the interval for currentTime
-        setTimeout(() => {
-          clearInterval(currentTimeInterval), 1000;
-        });
-        clearInterval(currentTimeInterval);
-        const targetBtn = event.currentTarget;
-        loadElementTime(targetBtn);
-      });
-
-      timezonePanelArray.filter(Boolean).map(function (item) {
-        return buttonContainer.appendChild(item);
-      });
-    });
+    lastTimezoneButton = timezoneButton;
   });
+
+  lastTimezoneButton.classList.add('current-timezone');
+  currentTimeZone = timezonesList[timezonesList.length - 1];
+  loadCurrentTimezoneTime();
+
+  localStorage.setItem("timezones_list", JSON.stringify(timezonesList));
 }
 
 // DOMContentLoaded event
 window.addEventListener("DOMContentLoaded", function () {
-  // To get the timezonePanelArray from localStorage and display it in the button container
-  let fetchedString = localStorage.getItem("localStorageTimezone");
-  let convertedArray = JSON.parse(fetchedString);
-  if (convertedArray === null || convertedArray.length === 0) {
-    // To update user's current time on the main screen
-    localStorage.clear();
-    userCurrentTime();
-    makeCurrentTimeButton();
-  } else {
-    populateTimezonePanel(convertedArray);
-  }
+  populateTimezonePanel(currentTimezones);
 });
-
-// To update user's current time on the main screen
-function userCurrentTime() {
-  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const date = new Date();
-  const time = date.toLocaleTimeString();
-
-  document.getElementById("timezone").textContent = timezone;
-  document.getElementById("time").textContent = time;
-  document.getElementById("date").textContent = date.toDateString();
-}
-const currentTimeInterval = setInterval(userCurrentTime, 1000);
-
-// make button in the button container panel
-function makeCurrentTimeButton() {
-  const timezoneName = document.getElementById("timezone").textContent;
-
-  const currentTime = new Date().toLocaleString("en-US", {
-    timeZone: timezoneName,
-    hour: "numeric",
-    minute: "numeric",
-    hour12: false,
-  });
-
-  const timezoneButton = document.createElement("button");
-  timezoneButton.setAttribute(
-    "class",
-    "flex justify-between py-3 px-5 text-left tabular-nums border-b ease-in-out hover:bg-amber-300 block w-full bg-gray-100"
-  );
-  timezoneButton.innerHTML =
-    '<span class="font-semibold">' +
-    currentTime +
-    '</span> <span class="text-gray-500 timezone-name">' +
-    timezoneName +
-    "</span>";
-
-  timezonePanelArray.push(timezoneButton);
-
-  timezonePanelArray.filter(Boolean).map(function (item) {
-    return buttonContainer.appendChild(item);
-  });
-}
 
 $(document).ready(function () {
   $(".se-select2").select2();
