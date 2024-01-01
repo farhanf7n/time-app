af234fb8fecc47671eb922653958f3e9c01fab84
Farhan Ahmed <farhanf7n@gmail.com>
2024-01-01 17:15:04 +0500

    Added functionality for start button

--- js/countDown.js
+++ js/countDown.js
@@ -11,6 +11,7 @@ const hourInput = document.getElementById("hour-input");
 const titleInput = document.getElementById("countdown-title-popup");
 const donePopupButton = document.querySelector(".done-timer-btn");
 const startBtn = document.querySelector(".start-timer-button");
+const resetBtn = document.querySelector(".reset-timer-button");
 
 const mainScreenTitle = document.querySelector(".main-screen-title");
 const addTimerPopup = document.getElementById("add-countdown-popup");
@@ -20,12 +21,12 @@ const closeSuccess = document.querySelector(".close-success-popup-btn");
 const testNotificationButton = document.getElementById("test-notification-btn");
 const closeTimerpopup = document.getElementById("close-popup-btn");
 
-let days = 0;
 let hour = 0;
 let minute = 0;
 let second = 0;
 let milliseconds = 0;
 let count = 0;
+let intervalId;
 
 // Fixes the leading zero issue
 function fixLeadingZero() {
@@ -36,7 +37,7 @@ function fixLeadingZero() {
 }
 
 // Stopwatch functionality
-function startStopWatch() {
+function startTimerFn() {
   milliseconds++;
   if (milliseconds == 100) {
     second++;
@@ -53,9 +54,10 @@ function startStopWatch() {
     minute = 0;
   }
 
-  if (hour == 24) {
-    days++;
-    hour = 0;
+  if (hour == 0 && minute == 0 && second == 0 && milliseconds == 0) {
+    console.log("Done");
+    clearInterval(intervalId);
+    successTimerPopup.classList.remove("hidden");
   }
 
   fixLeadingZero();
@@ -65,6 +67,11 @@ function startStopWatch() {
   secondsEl.innerHTML = second + ":";
   millisecondsEl.innerHTML = milliseconds;
 }
+
+// Start timer button
+startBtn.addEventListener("click", function () {
+  intervalId = setInterval(startTimerFn, 10);
+});
 
 // Closes the popup
 function closeTimezonePopup() {
@@ -109,16 +116,29 @@ donePopupButton.addEventListener("click", function () {
   mainScreenTitle.textContent = titleInput.value;
   if (hourInput.value) {
     hoursEl.innerHTML = hourInput.value + ": ";
+    hour = hourInput.value;
   } else {
     hoursEl.innerHTML = "00: ";
   }
 
   if (minuteInput.value) {
     minutesEl.innerHTML = minuteInput.value + ": ";
+    minute = minuteInput.value;
   } else {
     minutesEl.innerHTML = "00: ";
   }
 
   closeTimezonePopup();
   startBtn.classList.remove("hidden");
+});
+
+// Reset timer button
+resetBtn.addEventListener("click", function () {
+  hoursEl.textContent = "00: ";
+  minutesEl.textContent = "00: ";
+  secondsEl.textContent = "00: ";
+  millisecondsEl.textContent = "00";
+  startBtn.classList.add("hidden");
+  // Clear the interval
+  clearInterval(intervalId);
 });
